8a1781c1d2e601a0caededbd5ef1327048d58c56
Dave Walker <dave@dave.gs>
2014-06-20 15:33:28 +0100

    The test compatible with the function under test

--- 001-counting-words/test_count_words.py
+++ 001-counting-words/test_count_words.py
@@ -7,9 +7,9 @@ from count_words import count_words
 
 
 def test_count_words():
-    lines = """
-        Little pig, let me come in.
-        No, no, no, no, not by the hair on my little chin chin.
-    """
+    lines = [
+        "Little pig, let me come in.",
+        "No, no, no, no, not by the hair on my little chin chin.",
+    ]
     expected_top_three = [('no', 4), ('chin', 2), ('little', 2)]
     assert count_words(lines)[:3] == expected_top_three
