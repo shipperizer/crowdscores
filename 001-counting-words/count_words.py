1fb779cbf54cb02775c22884fbf0a689a6efe584
shipperizer <alexcabb@gmail.com>
2016-04-29 11:44:45 +0100

    first exercise

--- 001-counting-words/count_words.py
+++ 001-counting-words/count_words.py
@@ -1,12 +1,23 @@
+from collections import Counter
 import sys
+import re
+
+SPECIAL_CHARS = '[\*\.,;]'
 
 
 def count_words(lines):
-    pass
+    # strip special chars, lowercase and make it a list of words
+    lines = re.sub(SPECIAL_CHARS, '', ' '.join(lines)).lower().split()
+    # looks like that Counter.most_common gives back a different result sometimes
+    # like ('no', 4), ('chin', 2), ('little', 2) or ('no', 4), ('little', 2), ('chin', 2)
+    # with a ratio of 3:1 (tried 4 times), so going to sort the result
+    words = Counter(lines).most_common()
+    # sorting first by lexicographical order, the reverse sorting by number of occurences
+    return sorted(sorted(words, key=lambda x: x[0]), key=lambda x: x[1], reverse=True)
 
 
 if __name__ == '__main__':
     stream = sys.stdin
     lines = sys.stdin.readlines()
     for word, count in count_words(lines):
-        print word, count
+        print(word, count)
--- 001-counting-words/test_count_words.py
+++ 001-counting-words/test_count_words.py
@@ -2,14 +2,21 @@
 # (pip install pytest)
 # $> py.test -k test_count_words
 
-
+import pytest
 from count_words import count_words
 
 
-def test_count_words():
-    lines = [
-        "Little pig, let me come in.",
-        "No, no, no, no, not by the hair on my little chin chin.",
+@pytest.mark.parametrize('lines, result', [
+        (
+            ['Little pig, let me come in.', 'No, no, no, no, not by the hair on my little chin chin.'],
+            [('no', 4), ('chin', 2), ('little', 2)]
+        ),
+        (
+            ['Lorem ipsum dolor sit amet,', 'consectetur adipiscing elit; *sed do eiusmod tempor incididunt'],
+            [('adipiscing', 1), ('amet', 1), ('consectetur', 1)]
+        )
     ]
-    expected_top_three = [('no', 4), ('chin', 2), ('little', 2)]
+)
+def test_count_words(lines, result):
+    expected_top_three = result
     assert count_words(lines)[:3] == expected_top_three
